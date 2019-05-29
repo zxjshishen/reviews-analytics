7489dcdcc93f1460135a3f737627ab87f48bdeb8
zxjshishen <zxjshishen@163.com>
2019-05-29 20:22:44 +0800

    calc avg

--- read.py
+++ read.py
@@ -1,13 +1,16 @@
 data = []
 count = 0
+
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
 		if count % 1000 == 0:
 			print(len(data))
-print(len(data))
+print('档案读取完了，总共有', len(data), '笔资料')
 
-print(data[0])
-print('-----------------------')
-print(data[1])
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+	print(sum_len)
+print('留言的平均长度是', sum_len/len(data))
